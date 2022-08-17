ecaf90a924f27b1f24818d3713871d5ef81e4e1f
Isuru Fernando <isuruf@gmail.com>
2022-08-16 17:48:10 -0700

    remove static builds and remove Requires.private

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,10 +10,17 @@ mkdir $SRC_DIR/stage
             --without-harfbuzz \
             --with-bzip2=no \
             --with-brotli=no \
-            --enable-freetype-config
+            --enable-freetype-config \
+            --disable-static
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-make check
+if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" || "$CROSSCOMPILING_EMULATOR" != "" ]]; then
+    make check
 fi
 make install
+
+# Remove Requires.private as it is not needed for shared libraries.
+# A pkg-config bug requires the Requires.private pc files to be
+# present even for shared build
+sed -i.bak '/^Requires\.private/d' $SRC_DIR/stage/lib/pkgconfig/freetype2.pc
+rm $SRC_DIR/stage/lib/pkgconfig/freetype2.pc.bak
