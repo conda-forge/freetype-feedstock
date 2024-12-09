11d2d561552b675e13c3d8621a1460bab5d2e89c
Daniel Ching <dching@nvidia.com>
2024-12-09 14:02:50 -0600

    REF: Revert staging directory; use conda-build include/exclude

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,12 @@
 set CXXFLAGS=
 set CFLAGS=
 
-mkdir %SRC_DIR%\stage
-
 mkdir build
 cd build
 
 :: Configure.
 cmake -G"Ninja" ^
-      -DCMAKE_INSTALL_PREFIX:PATH=%SRC_DIR%\stage ^
+      -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX:/=\\%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%:/=\\" ^
       -DCMAKE_SYSTEM_PREFIX_PATH="%LIBRARY_PREFIX:/=\\%" ^
@@ -36,16 +34,9 @@ cmake --build . --config Release --target install
 if errorlevel 1 exit 1
 
 :: Move everything 1-level down.
-move %SRC_DIR%\stage\include\freetype2\freetype %SRC_DIR%\stage\include || exit 1
-move %SRC_DIR%\stage\include\freetype2\ft2build.h %SRC_DIR%\stage\include || exit 1
+move %LIBRARY_INC%\freetype2\freetype %LIBRARY_INC% || exit 1
+move %LIBRARY_INC%\freetype2\ft2build.h %LIBRARY_INC% || exit 1
 
 :: vs2008 created libfreetype.dll instead of freetype.dll
-set LIB="%SRC_DIR%\stage\bin\libfreetype.dll"
-if exist %LIB% (copy %LIB% %SRC_DIR%\stage\bin\freetype.dll) || exit 1
-
-setlocal EnableExtensions ENABLEDELAYEDEXPANSION
-for %%f in ( "%SRC_DIR%\stage\lib\pkgconfig\*.pc" ) do (
-    sed -i.bak "s,prefix=.*,prefix=/opt/anaconda1anaconda2anaconda3/Library,g" %%f
-    del %%f.bak
-)
-endlocal
+set LIB="%LIBRARY_BIN%\libfreetype.dll"
+if exist %LIB% (copy %LIB% %LIBRARY_BIN%\freetype.dll) || exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,7 @@
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./builds/unix
 
-mkdir $SRC_DIR/stage
-
-./configure --prefix=${SRC_DIR}/stage \
+./configure --prefix=${PREFIX} \
             --with-zlib=yes \
             --with-png=yes \
             --without-harfbuzz \
@@ -22,12 +20,9 @@ make install
 # Remove Requires.private as it is not needed for shared libraries.
 # A pkg-config bug requires the Requires.private pc files to be
 # present even for shared build
-sed -i.bak '/^Requires\.private/d' $SRC_DIR/stage/lib/pkgconfig/freetype2.pc
-sed -i.bak "s,$SRC_DIR/stage,/opt/anaconda1anaconda2anaconda3,g" $SRC_DIR/stage/lib/pkgconfig/*.pc
-rm $SRC_DIR/stage/lib/pkgconfig/*.bak
+sed -i.bak '/^Requires\.private/d' $PREFIX/lib/pkgconfig/freetype2.pc
+rm $PREFIX/lib/pkgconfig/*.bak
 
-# Use placeholder for stage paths in freetype-config
-sed -i.bak "s,$SRC_DIR/stage,$PREFIX,g" $SRC_DIR/stage/bin/freetype-config
 # Use conda pkg-config
-sed -i.bak "s,/usr/bin/,$PREFIX/bin/,g" $SRC_DIR/stage/bin/freetype-config
-rm $SRC_DIR/stage/bin/freetype-config.bak
+sed -i.bak "s,/usr/bin/,$PREFIX/bin/,g" $PREFIX/bin/freetype-config
+rm $PREFIX/bin/freetype-config.bak
